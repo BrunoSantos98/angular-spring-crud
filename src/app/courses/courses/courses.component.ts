7a0a74d9551857cbc51c0117b9747888cb5b0964
bruno santos <bruno4t@hotmail.com>
2022-11-24 18:29:23 -0300

    criaação de service e module

--- src/app/courses/courses/courses.component.ts
+++ src/app/courses/courses/courses.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Course } from '../models/course';
+import { CoursesService } from '../services/courses.service';
 
 @Component({
   selector: 'app-courses',
@@ -6,5 +8,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./courses.component.scss']
 })
 export class CoursesComponent {
+
+  courses:Course[] = [];
+  displayedColumns = ['name','category'];
+
+  coursesService: CoursesService;
+
+  constructor(){
+
+    this.coursesService = new CoursesService();
+    this.courses = this.coursesService.list();
+  }
 
 }
